7baae5449b5383ee3280265ba6277bce0ad97eef
Diogo Marcelo <marcello.razer@gmail.com>
2022-06-03 00:12:04 -0300

    Melhorias na formatação de relatorio

--- src/relatorios/GeradorDeRelatorios.java
+++ src/relatorios/GeradorDeRelatorios.java
@@ -5,12 +5,16 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Calendar;
 
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.Image;
 import com.itextpdf.text.PageSize;
 import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Font.FontFamily;
 import com.itextpdf.text.pdf.PdfWriter;
 
 import classes.Canal;
@@ -19,62 +23,44 @@ import persistencia.CentralDeInformacoes;
 import persistencia.Persistencia;
 
 public class GeradorDeRelatorios {
+	
 	private static File arquivo = new File(System.getProperty("user.dir") + "/relatorios.pdf");
-	
-	private static File programacaoDeHoje = new File(System.getProperty("user.dir") + "/relatorios-hoje.pdf");
-	
-	
 	
 	
 	public static void gerarProgramacaoDeHoje(CentralDeInformacoes central) throws Exception {
-			
-		String dados = "Programas exibidos hoje: \n";
-		
-		Document doc = new Document(PageSize.A4);
-		
-		Paragraph paragraph = new Paragraph();
-		
-		LocalDate localDate = LocalDate.now();
-		
-		int hoje = DayOfWeek.from(localDate).getValue();
-		
-		
-		
-		try {
+		Document programacao = new Document(PageSize.A4);
+		PdfWriter.getInstance(programacao, new FileOutputStream("relatorios-hoje.pdf"));
+		programacao.open();
 
-			FileOutputStream out;
-			if (programacaoDeHoje.exists()) {
-				out = new FileOutputStream(programacaoDeHoje);
-			} else {
-				out = new FileOutputStream("relatorios-hoje.pdf");
+		Image logoDoProjeto = Image.getInstance("imagens/mySeries.png");
+		logoDoProjeto.setAlignment(1);
+		programacao.add(logoDoProjeto);
 
-			}
+		Font fonteTitulo = new Font(FontFamily.HELVETICA, 16, Font.BOLD);
+		Paragraph titulo = new Paragraph("- - - - PROGRAMAÇÃO - - - -", fonteTitulo);
+		titulo.setAlignment(1);
+		programacao.add(titulo);
 
-			PdfWriter.getInstance(doc, out);
-			doc.open();
-			
-			for (ProgramaDeTV programas : central.getTodosOsProgramas()) {
-				if (programas.getCanal() != null) {
-					if(programas.getDiasDaSemana().getValue() == hoje) {
-						dados += "\n" + programas.getNome();
-					}
+		LocalDate dataAtual = LocalDate.now();
+
+		int isHoje = DayOfWeek.from(dataAtual).getValue();
+
+		Paragraph dadosDataAtual = new Paragraph(dataAtual.toString());
+		dadosDataAtual.setAlignment(1);
+		programacao.add(dadosDataAtual);
+		ArrayList<ProgramaDeTV> todosOsProgramas = central.getTodosOsProgramas();
+		for (ProgramaDeTV programa : todosOsProgramas) {
+			if (programa.getCanal() != null) {
+				if (programa.getDiasDaSemana().getValue() == isHoje) {
+					Paragraph nomeDoPrograma = new Paragraph(
+							"\nPrograma: " + programa.getNome() + "\nCanal: " + programa.getCanal().getNomeDoCanal());
+					programacao.add(nomeDoPrograma);		
 				}
 			}
-
-		} catch (FileNotFoundException e) {
-
-			e.printStackTrace();
-		} catch (DocumentException e) {
-
-			e.printStackTrace();
 		}
-		Paragraph dadoP = new Paragraph(dados);
-		doc.add(paragraph);
-		doc.add(dadoP);
-		doc.close();
 		
+		programacao.close();
 	}
-	
 	
 
 	public static void obterProgramacaoDeUmCanal(Canal canal) throws Exception {
